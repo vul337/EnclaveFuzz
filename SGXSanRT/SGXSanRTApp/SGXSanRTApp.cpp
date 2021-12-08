82d451745032c98abe21e9d17e34d9b6c44bf6c1
LihengChen <791960492@qq.com>
2021-12-08 16:18:00 +0800

    remove dependency on sanitizer_common

--- SGXSanRT/SGXSanRTApp/SGXSanRTApp.cpp
+++ SGXSanRT/SGXSanRTApp/SGXSanRTApp.cpp
@@ -5,6 +5,7 @@
 #include <signal.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 #include "SGXSanManifest.h"
 #include "SGXSanRTApp.hpp"
 #include "SGXSanCommonShadowMap.hpp"
@@ -21,165 +22,147 @@
 #define xstr(s) str(s)
 #define str(s) #s
 
-namespace __sanitizer
-{
-       uptr GetMmapGranularity();
-
-       // Used to check if we can map shadow memory to a fixed location.
-       bool MemoryRangeIsAvailable(uptr range_start, uptr range_end);
-
-       // Reserve memory range [beg, end]. If madvise_shadow is true then apply
-       // madvise (e.g. hugepages, core dumping) requested by options.
-       void ReserveShadowMemoryRange(uptr beg, uptr end, const char *name,
-                                     bool madvise_shadow = true);
-}
-
 uptr g_enclave_base = 0, g_enclave_size = 0;
 uint64_t kLowMemBeg = 0, kLowMemEnd = 0,
-         kLowShadowBeg = 0, kLowShadowEnd = 0,
-         kShadowGapBeg = 0, kShadowGapEnd = 0,
-         kHighShadowBeg = 0, kHighShadowEnd = 0,
-         kHighMemBeg = 0, kHighMemEnd = 0;
+		 kLowShadowBeg = 0, kLowShadowEnd = 0,
+		 kShadowGapBeg = 0, kShadowGapEnd = 0,
+		 kHighShadowBeg = 0, kHighShadowEnd = 0,
+		 kHighMemBeg = 0, kHighMemEnd = 0;
 static uint64_t g_enclave_low_guard_start = 0, g_enclave_high_guard_end = 0;
 static struct sigaction g_old_sigact[_NSIG];
 void PrintAddressSpaceLayout()
 {
-       printf("|| `[%16p, %16p]` || Shadow    ||\n", (void *)kLowShadowBeg, (void *)kLowShadowEnd);
-       printf("|| `[%16p, %16p]` || LowGuard  ||\n", (void *)g_enclave_low_guard_start, (void *)(g_enclave_base - 1));
-       printf("|| `[%16p, %16p]` || Elrange   ||\n", (void *)g_enclave_base, (void *)(g_enclave_base + g_enclave_size - 1));
-       printf("|| `[%16p, %16p]` || HighGuard ||\n\n", (void *)(g_enclave_base + g_enclave_size), (void *)g_enclave_high_guard_end);
+	printf("|| `[%16p, %16p]` || Shadow    ||\n", (void *)kLowShadowBeg, (void *)kLowShadowEnd);
+	printf("|| `[%16p, %16p]` || LowGuard  ||\n", (void *)g_enclave_low_guard_start, (void *)(g_enclave_base - 1));
+	printf("|| `[%16p, %16p]` || Elrange   ||\n", (void *)g_enclave_base, (void *)(g_enclave_base + g_enclave_size - 1));
+	printf("|| `[%16p, %16p]` || HighGuard ||\n\n", (void *)(g_enclave_base + g_enclave_size), (void *)g_enclave_high_guard_end);
 }
 
 void sgxsan_sigaction(int signum, siginfo_t *siginfo, void *priv)
 {
-       ABORT_ASSERT(signum == SIGSEGV, "Currently only regist SIGSEGV handler");
+	ABORT_ASSERT(signum == SIGSEGV, "Currently only regist SIGSEGV handler");
 
-       // process siginfo
-       uint64_t page_fault_addr = (uint64_t)siginfo->si_addr;
-       if ((void *)page_fault_addr == nullptr)
-       {
-              printf("[SGXSAN] Null-Pointer dereference\n");
-       }
-       else if ((g_enclave_low_guard_start <= page_fault_addr && page_fault_addr < g_enclave_base) ||
-                ((g_enclave_base + g_enclave_size) <= page_fault_addr && page_fault_addr <= g_enclave_high_guard_end))
-       {
-              printf("[SGXSAN] Pointer dereference overflows enclave boundray\n");
-       }
-       else if ((g_enclave_low_guard_start <= page_fault_addr && page_fault_addr < g_enclave_base) ||
-                ((g_enclave_base + g_enclave_size - 0x1000) <= page_fault_addr && page_fault_addr < (g_enclave_base + g_enclave_size)))
-       {
-              printf("[SGXSAN] Infer pointer dereference overflows enclave boundray, as mprotect's effort is page-granularity and si_addr only give page-granularity address\n");
-       }
+	// process siginfo
+	uint64_t page_fault_addr = (uint64_t)siginfo->si_addr;
+	if ((void *)page_fault_addr == nullptr)
+	{
+		printf("[SGXSAN] Null-Pointer dereference\n");
+	}
+	else if ((g_enclave_low_guard_start <= page_fault_addr && page_fault_addr < g_enclave_base) ||
+			 ((g_enclave_base + g_enclave_size) <= page_fault_addr && page_fault_addr <= g_enclave_high_guard_end))
+	{
+		printf("[SGXSAN] Pointer dereference overflows enclave boundray\n");
+	}
+	else if ((g_enclave_low_guard_start <= page_fault_addr && page_fault_addr < g_enclave_base) ||
+			 ((g_enclave_base + g_enclave_size - 0x1000) <= page_fault_addr && page_fault_addr < (g_enclave_base + g_enclave_size)))
+	{
+		printf("[SGXSAN] Infer pointer dereference overflows enclave boundray, as mprotect's effort is page-granularity and si_addr only give page-granularity address\n");
+	}
 
-       // call previous signal handler
-       if (SIG_DFL == g_old_sigact[signum].sa_handler)
-       {
-              signal(signum, SIG_DFL);
-              raise(signum);
-       }
-       //if there is old signal handler, we need transfer the signal to the old signal handler;
-       else
-       {
-              // make sure signum to be masked if SA_NODEFER is not set
-              if (!(g_old_sigact[signum].sa_flags & SA_NODEFER))
-                     sigaddset(&g_old_sigact[signum].sa_mask, signum);
-              // use mask of old sigact
-              sigset_t cur_set;
-              pthread_sigmask(SIG_SETMASK, &g_old_sigact[signum].sa_mask, &cur_set);
+	// call previous signal handler
+	if (SIG_DFL == g_old_sigact[signum].sa_handler)
+	{
+		signal(signum, SIG_DFL);
+		raise(signum);
+	}
+	//if there is old signal handler, we need transfer the signal to the old signal handler;
+	else
+	{
+		// make sure signum to be masked if SA_NODEFER is not set
+		if (!(g_old_sigact[signum].sa_flags & SA_NODEFER))
+			sigaddset(&g_old_sigact[signum].sa_mask, signum);
+		// use mask of old sigact
+		sigset_t cur_set;
+		pthread_sigmask(SIG_SETMASK, &g_old_sigact[signum].sa_mask, &cur_set);
 
-              if (g_old_sigact[signum].sa_flags & SA_SIGINFO)
-              {
-                     g_old_sigact[signum].sa_sigaction(signum, siginfo, priv);
-              }
-              else
-              {
-                     g_old_sigact[signum].sa_handler(signum);
-              }
+		if (g_old_sigact[signum].sa_flags & SA_SIGINFO)
+		{
+			g_old_sigact[signum].sa_sigaction(signum, siginfo, priv);
+		}
+		else
+		{
+			g_old_sigact[signum].sa_handler(signum);
+		}
 
-              pthread_sigmask(SIG_SETMASK, &cur_set, NULL);
+		pthread_sigmask(SIG_SETMASK, &cur_set, NULL);
 
-              //If the g_old_sigact set SA_RESETHAND, it will break the chain which means
-              //g_old_sigact->next_old_sigact will not be called. Our signal handler does not
-              //responsable for that. We just follow what os do on SA_RESETHAND.
-              if (g_old_sigact[signum].sa_flags & SA_RESETHAND)
-                     g_old_sigact[signum].sa_handler = SIG_DFL;
-       }
+		//If the g_old_sigact set SA_RESETHAND, it will break the chain which means
+		//g_old_sigact->next_old_sigact will not be called. Our signal handler does not
+		//responsable for that. We just follow what os do on SA_RESETHAND.
+		if (g_old_sigact[signum].sa_flags & SA_RESETHAND)
+			g_old_sigact[signum].sa_handler = SIG_DFL;
+	}
 }
 
 void reg_sgxsan_sigaction()
 {
-       struct sigaction sig_act;
-       memset(&sig_act, 0, sizeof(sig_act));
-       sig_act.sa_sigaction = sgxsan_sigaction;
-       sig_act.sa_flags = SA_SIGINFO | SA_NODEFER | SA_RESTART;
-       sigemptyset(&sig_act.sa_mask);
-       ABORT_ASSERT(0 == sigprocmask(SIG_SETMASK, NULL, &sig_act.sa_mask), "Fail to get signal mask");
-       // make sure SIGSEGV is not blocked
-       sigdelset(&sig_act.sa_mask, SIGSEGV);
-       // take place before signal handler of sgx aex
-       ABORT_ASSERT(0 == sigaction(SIGSEGV, &sig_act, &g_old_sigact[SIGSEGV]), "Fail to regist SIGSEGV action");
+	struct sigaction sig_act;
+	memset(&sig_act, 0, sizeof(sig_act));
+	sig_act.sa_sigaction = sgxsan_sigaction;
+	sig_act.sa_flags = SA_SIGINFO | SA_NODEFER | SA_RESTART;
+	sigemptyset(&sig_act.sa_mask);
+	ABORT_ASSERT(0 == sigprocmask(SIG_SETMASK, NULL, &sig_act.sa_mask), "Fail to get signal mask");
+	// make sure SIGSEGV is not blocked
+	sigdelset(&sig_act.sa_mask, SIGSEGV);
+	// take place before signal handler of sgx aex
+	ABORT_ASSERT(0 == sigaction(SIGSEGV, &sig_act, &g_old_sigact[SIGSEGV]), "Fail to regist SIGSEGV action");
 }
 
 // create shadow memory outside enclave for elrange
 // because shadow is independent of elrange, we just need one block of memory for shadow, and don't need consider shadow gap.
 void ocall_init_shadow_memory(uptr enclave_base, uptr enclave_size, uptr *shadow_beg_ptr, uptr *shadow_end_ptr)
 {
-       g_enclave_base = enclave_base;
-       g_enclave_size = enclave_size;
+	g_enclave_base = enclave_base;
+	g_enclave_size = enclave_size;
 
-       // only use LowMem and LowShadow as ELRANGE and EnclaveShadow
-       kLowShadowBeg = SGXSAN_SHADOW_MAP_BASE;
-       kLowShadowEnd = (enclave_size >> 3) + kLowShadowBeg - 1;
-       kLowMemBeg = g_enclave_base;
-       kLowMemEnd = g_enclave_base + enclave_size - 1;
+	// only use LowMem and LowShadow as ELRANGE and EnclaveShadow
+	kLowShadowBeg = SGXSAN_SHADOW_MAP_BASE;
+	kLowShadowEnd = (enclave_size >> 3) + kLowShadowBeg - 1;
+	kLowMemBeg = g_enclave_base;
+	kLowMemEnd = g_enclave_base + enclave_size - 1;
 
-       uptr shadow_start = kLowShadowBeg;
+	uptr shadow_start = kLowShadowBeg;
 
-       bool full_shadow_is_available = false;
+	uptr page_size = getpagesize();
+	ABORT_ASSERT(page_size == 0x1000, "Currently only support 4k page size");
+	shadow_start -= page_size;
 
-       uptr page_size = __sanitizer::GetMmapGranularity();
-       ABORT_ASSERT(page_size == (1 << 12), "Currently only support 4k page size");
-       shadow_start -= page_size;
+	// fix-me: may need unmap at destructor
+	// mmap the shadow plus at least one page at the left.
+	ABORT_ASSERT((MAP_FAILED != mmap((void *)shadow_start, kLowShadowEnd - shadow_start + 1, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_FIXED | MAP_NORESERVE | MAP_ANON, -1, 0)) &&
+					 (-1 != madvise((void *)shadow_start, kLowShadowEnd - shadow_start + 1, MADV_NOHUGEPAGE)),
+				 "Shadow Memory unavailable");
 
-       if (!full_shadow_is_available)
-              full_shadow_is_available =
-                  __sanitizer::MemoryRangeIsAvailable(shadow_start, kLowShadowEnd);
-       ABORT_ASSERT(full_shadow_is_available, "Shadow is unavailable");
+	ABORT_ASSERT(((kLowMemBeg & 0xfff) == 0) && (((kLowMemEnd + 1) & 0xfff) == 0), "Elrange is not aligned to page");
 
-       // fix-me: may need unmap at destructor
-       // mmap the shadow plus at least one page at the left.
-       __sanitizer::ReserveShadowMemoryRange(shadow_start, kLowShadowEnd, "shadow");
+	// consistent with modification in psw/enclave_common/sgx_enclave_common.cpp:enclave_create_ex
+	g_enclave_low_guard_start = kLowMemBeg - page_size;
+	g_enclave_high_guard_end = kLowMemEnd + page_size;
+	// ABORT_ASSERT((MAP_FAILED != mmap((void *)g_enclave_low_guard_start, kLowMemBeg - g_enclave_low_guard_start, PROT_NONE, MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) ||
+	//                  (MAP_FAILED != mmap((void *)(kLowMemEnd + 1), g_enclave_high_guard_end - kLowMemEnd, PROT_NONE, MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)),
+	//              "ElrangeGuard unavailable");
 
-       ABORT_ASSERT(((kLowMemBeg & 0xfff) == 0) && (((kLowMemEnd + 1) & 0xfff) == 0), "Elrange is not aligned to page");
+	PrintAddressSpaceLayout();
 
-       // consistent with modification in psw/enclave_common/sgx_enclave_common.cpp:enclave_create_ex
-       g_enclave_low_guard_start = kLowMemBeg - page_size;
-       g_enclave_high_guard_end = kLowMemEnd + page_size;
-       // ABORT_ASSERT((MAP_FAILED != mmap((void *)g_enclave_low_guard_start, kLowMemBeg - g_enclave_low_guard_start, PROT_NONE, MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) ||
-       //                  (MAP_FAILED != mmap((void *)(kLowMemEnd + 1), g_enclave_high_guard_end - kLowMemEnd, PROT_NONE, MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)),
-       //              "ElrangeGuard unavailable");
+	*shadow_beg_ptr = kLowShadowBeg;
+	*shadow_end_ptr = kLowShadowEnd;
 
-       PrintAddressSpaceLayout();
+	// start shallow poison on sensitive layout
+	SGXSanEnclaveConfigReader reader{g_enclave_base};
+	// printf("ENCLAVE_FILENAME=%s\n", xstr(ENCLAVE_FILENAME));
+	reader.collect_layout_infos(xstr(ENCLAVE_FILENAME));
+	reader.shallow_poison_senitive();
 
-       *shadow_beg_ptr = kLowShadowBeg;
-       *shadow_end_ptr = kLowShadowEnd;
+	// memset((void *)(kLowShadowBeg - page_size), kSGXSanElrangeLeftGuard, page_size);
 
-       // start shallow poison on sensitive layout
-       SGXSanEnclaveConfigReader reader{g_enclave_base};
-       // printf("ENCLAVE_FILENAME=%s\n", xstr(ENCLAVE_FILENAME));
-       reader.collect_layout_infos(xstr(ENCLAVE_FILENAME));
-       reader.shallow_poison_senitive();
-
-       // memset((void *)(kLowShadowBeg - page_size), kSGXSanElrangeLeftGuard, page_size);
-
-       reg_sgxsan_sigaction();
+	reg_sgxsan_sigaction();
 }
 
 /* OCall functions */
 extern "C" void sgxsan_ocall_print_string(const char *str)
 {
-    /* Proxy/Bridge will check the length and null-terminate 
+	/* Proxy/Bridge will check the length and null-terminate 
      * the input string to prevent buffer overflow. 
      */
-    printf("%s", str);
+	printf("%s", str);
 }
--- SGXSanRT/SGXSanRTEnclave/Malloc.cpp
+++ SGXSanRT/SGXSanRTEnclave/Malloc.cpp
@@ -46,203 +46,203 @@ static std::unordered_set<uptr> heap_obj_user_beg_set;
 
 struct chunk
 {
-  uptr alloc_beg;
-  size_t user_size;
+	uptr alloc_beg;
+	size_t user_size;
 };
 
 void *MALLOC(size_t size)
 {
-  if (not asan_inited)
-  {
-    return BACKEND_MALLOC(size);
-  }
+	if (not asan_inited)
+	{
+		return BACKEND_MALLOC(size);
+	}
 
-  uptr alignment = SHADOW_GRANULARITY;
+	uptr alignment = SHADOW_GRANULARITY;
 
-  if (size == 0)
-  {
-    return nullptr;
-  }
+	if (size == 0)
+	{
+		return nullptr;
+	}
 
-  uptr rz_size = ComputeRZSize(size);
-  uptr rounded_size = RoundUpTo(size, alignment);
-  uptr needed_size = rounded_size + 2 * rz_size;
+	uptr rz_size = ComputeRZSize(size);
+	uptr rounded_size = RoundUpTo(size, alignment);
+	uptr needed_size = rounded_size + 2 * rz_size;
 
-  void *allocated = BACKEND_MALLOC(needed_size);
+	void *allocated = BACKEND_MALLOC(needed_size);
 
-  if (allocated == nullptr)
-  {
-    return nullptr;
-  }
+	if (allocated == nullptr)
+	{
+		return nullptr;
+	}
 
-  uptr alloc_beg = reinterpret_cast<uptr>(allocated);
-  // If dlmalloc doesn't return an aligned memory, it's troublesome.
-  // If it is so, we start to posion from RoundUpTo(allocated)
-  assert(IsAligned(alloc_beg, alignment) && "here I want to see whether dlmalloc return an unaligned memory");
-  uptr alloc_end = alloc_beg + needed_size;
+	uptr alloc_beg = reinterpret_cast<uptr>(allocated);
+	// If dlmalloc doesn't return an aligned memory, it's troublesome.
+	// If it is so, we start to posion from RoundUpTo(allocated)
+	assert(IsAligned(alloc_beg, alignment) && "here I want to see whether dlmalloc return an unaligned memory");
+	uptr alloc_end = alloc_beg + needed_size;
 
-  uptr user_beg = alloc_beg + rz_size;
-  if (!IsAligned(user_beg, alignment))
-    user_beg = RoundUpTo(user_beg, alignment);
-  uptr user_end = user_beg + size;
-  CHECK_LE(user_end, alloc_end);
+	uptr user_beg = alloc_beg + rz_size;
+	if (!IsAligned(user_beg, alignment))
+		user_beg = RoundUpTo(user_beg, alignment);
+	uptr user_end = user_beg + size;
+	CHECK_LE(user_end, alloc_end);
 
-  // place the chunk in left redzone
-  uptr chunk_beg = user_beg - sizeof(chunk);
-  chunk *m = reinterpret_cast<chunk *>(chunk_beg);
+	// place the chunk in left redzone
+	uptr chunk_beg = user_beg - sizeof(chunk);
+	chunk *m = reinterpret_cast<chunk *>(chunk_beg);
 
-  // if alloc_beg is not aligned, we cannot automatically calculate it
-  m->alloc_beg = alloc_beg;
-  m->user_size = size;
+	// if alloc_beg is not aligned, we cannot automatically calculate it
+	m->alloc_beg = alloc_beg;
+	m->user_size = size;
 
-  // PRINTF("\n[malloc] alloc_beg=0x%lx user_beg=0x%lx\n", alloc_beg, user_beg);
+	// PRINTF("\n[malloc] alloc_beg=0x%lx user_beg=0x%lx\n", alloc_beg, user_beg);
 
-  // start poisoning
-  // if assume alloc_beg is 8-byte aligned, we can use FastPoisonShadow()
-  /* Fast */ PoisonShadow(alloc_beg, user_beg - alloc_beg, kAsanHeapLeftRedzoneMagic);
-  PoisonShadow(user_beg, size, 0x0); // user_beg is already aligned to alignment
-  uptr right_redzone_beg = RoundUpTo(user_end, alignment);
-  /* Fast */ PoisonShadow(right_redzone_beg, alloc_end - right_redzone_beg, kAsanHeapRightRedzoneMagic);
+	// start poisoning
+	// if assume alloc_beg is 8-byte aligned, we can use FastPoisonShadow()
+	/* Fast */ PoisonShadow(alloc_beg, user_beg - alloc_beg, kAsanHeapLeftRedzoneMagic);
+	PoisonShadow(user_beg, size, 0x0); // user_beg is already aligned to alignment
+	uptr right_redzone_beg = RoundUpTo(user_end, alignment);
+	/* Fast */ PoisonShadow(right_redzone_beg, alloc_end - right_redzone_beg, kAsanHeapRightRedzoneMagic);
 
-  // PRINTF("[heap_obj_user_beg_set] [before malloc] ");
-  // for (uptr p : heap_obj_user_beg_set)
-  // {
-  //     PRINTF(" %lx", p);
-  // }
-  // PRINTF(" %s", "\n");
+	// PRINTF("[heap_obj_user_beg_set] [before malloc] ");
+	// for (uptr p : heap_obj_user_beg_set)
+	// {
+	//     PRINTF(" %lx", p);
+	// }
+	// PRINTF(" %s", "\n");
 
-  // record user_beg avoid user passing incorrect addr to free
-  // I assume dlmalloc will not alloc an memory that already allocated
-  // also assume thread safety implemented by dlmalloc, then alloc_beg never be the same for multi-threads
-  // fix-me: is container thread-safe? (https://en.cppreference.com/w/cpp/container)
+	// record user_beg avoid user passing incorrect addr to free
+	// I assume dlmalloc will not alloc an memory that already allocated
+	// also assume thread safety implemented by dlmalloc, then alloc_beg never be the same for multi-threads
+	// fix-me: is container thread-safe? (https://en.cppreference.com/w/cpp/container)
 #if (CHECK_MALLOC_FREE_MATCH)
-  pthread_rwlock_wrlock(&rwlock_heap_obj_user_beg_set);
-  if (heap_obj_user_beg_set.find(user_beg) == heap_obj_user_beg_set.end())
-  {
-    heap_obj_user_beg_set.insert(user_beg);
-    pthread_rwlock_unlock(&rwlock_heap_obj_user_beg_set);
-  }
-  else
-  {
-    pthread_rwlock_unlock(&rwlock_heap_obj_user_beg_set);
-    PrintErrorAndAbort("malloc an already allocated memory");
-  }
+	pthread_rwlock_wrlock(&rwlock_heap_obj_user_beg_set);
+	if (heap_obj_user_beg_set.find(user_beg) == heap_obj_user_beg_set.end())
+	{
+		heap_obj_user_beg_set.insert(user_beg);
+		pthread_rwlock_unlock(&rwlock_heap_obj_user_beg_set);
+	}
+	else
+	{
+		pthread_rwlock_unlock(&rwlock_heap_obj_user_beg_set);
+		PrintErrorAndAbort("malloc an already allocated memory");
+	}
 #endif
-  // PRINTF("[heap_obj_user_beg_set] [after malloc] ");
-  // for (uptr p : heap_obj_user_beg_set)
-  // {
-  //     PRINTF(" %lx", p);
-  // }
-  // PRINTF(" %s", "\n");
+	// PRINTF("[heap_obj_user_beg_set] [after malloc] ");
+	// for (uptr p : heap_obj_user_beg_set)
+	// {
+	//     PRINTF(" %lx", p);
+	// }
+	// PRINTF(" %s", "\n");
 
-  return reinterpret_cast<void *>(user_beg);
+	return reinterpret_cast<void *>(user_beg);
 }
 
 void FREE(void *ptr)
 {
-  if (not asan_inited)
-  {
-    BACKEND_FREE(ptr);
-    return;
-  }
+	if (not asan_inited)
+	{
+		BACKEND_FREE(ptr);
+		return;
+	}
 
-  uptr user_beg = reinterpret_cast<uptr>(ptr);
-  uptr alignment = SHADOW_GRANULARITY;
-  CHECK(IsAligned(user_beg, alignment));
+	uptr user_beg = reinterpret_cast<uptr>(ptr);
+	uptr alignment = SHADOW_GRANULARITY;
+	CHECK(IsAligned(user_beg, alignment));
 
-  // PRINTF("[heap_obj_user_beg_set] [before free] ");
-  // for (uptr p : heap_obj_user_beg_set)
-  // {
-  //     PRINTF(" %lx", p);
-  // }
-  // PRINTF(" %s", "\n");
+	// PRINTF("[heap_obj_user_beg_set] [before free] ");
+	// for (uptr p : heap_obj_user_beg_set)
+	// {
+	//     PRINTF(" %lx", p);
+	// }
+	// PRINTF(" %s", "\n");
 #if (CHECK_MALLOC_FREE_MATCH)
-  pthread_rwlock_wrlock(&rwlock_heap_obj_user_beg_set);
-  if (heap_obj_user_beg_set.find(user_beg) == heap_obj_user_beg_set.end())
-  {
-    pthread_rwlock_unlock(&rwlock_heap_obj_user_beg_set);
-    PrintErrorAndAbort("free an non-recorded address"); // abort();
-  }
-  else
-  {
-    heap_obj_user_beg_set.erase(user_beg);
-    pthread_rwlock_unlock(&rwlock_heap_obj_user_beg_set);
-  }
+	pthread_rwlock_wrlock(&rwlock_heap_obj_user_beg_set);
+	if (heap_obj_user_beg_set.find(user_beg) == heap_obj_user_beg_set.end())
+	{
+		pthread_rwlock_unlock(&rwlock_heap_obj_user_beg_set);
+		PrintErrorAndAbort("free an non-recorded address"); // abort();
+	}
+	else
+	{
+		heap_obj_user_beg_set.erase(user_beg);
+		pthread_rwlock_unlock(&rwlock_heap_obj_user_beg_set);
+	}
 #endif
-  uptr chunk_beg = user_beg - sizeof(chunk);
-  chunk *m = reinterpret_cast<chunk *>(chunk_beg);
-  size_t user_size = m->user_size;
-  // PRINTF("\n[recycle] alloc_beg=0x%lx user_beg=0x%lx\n", m->alloc_beg, user_beg);
-  FastPoisonShadow(user_beg, RoundUpTo(user_size, alignment), kAsanHeapFreeMagic);
+	uptr chunk_beg = user_beg - sizeof(chunk);
+	chunk *m = reinterpret_cast<chunk *>(chunk_beg);
+	size_t user_size = m->user_size;
+	// PRINTF("\n[recycle] alloc_beg=0x%lx user_beg=0x%lx\n", m->alloc_beg, user_beg);
+	FastPoisonShadow(user_beg, RoundUpTo(user_size, alignment), kAsanHeapFreeMagic);
 
-  QuarantineElement qe = {
-      .alloc_beg = m->alloc_beg,
-      .alloc_size = ComputeRZSize(user_size) * 2 + RoundUpTo(user_size, alignment),
-      .user_beg = user_beg,
-      .user_size = user_size};
-  g_quarantine_cache->put(qe);
+	QuarantineElement qe = {
+		.alloc_beg = m->alloc_beg,
+		.alloc_size = ComputeRZSize(user_size) * 2 + RoundUpTo(user_size, alignment),
+		.user_beg = user_beg,
+		.user_size = user_size};
+	g_quarantine_cache->put(qe);
 
-  // PRINTF("[heap_obj_user_beg_set] [after free] ");
-  // for (uptr p : heap_obj_user_beg_set)
-  // {
-  //     PRINTF(" %lx", p);
-  // }
-  // PRINTF(" %s", "\n");
+	// PRINTF("[heap_obj_user_beg_set] [after free] ");
+	// for (uptr p : heap_obj_user_beg_set)
+	// {
+	//     PRINTF(" %lx", p);
+	// }
+	// PRINTF(" %s", "\n");
 }
 
 void *CALLOC(size_t n_elements, size_t elem_size)
 {
-  if (not asan_inited)
-  {
-    return BACKEND_CALLOC(n_elements, elem_size);
-  }
+	if (not asan_inited)
+	{
+		return BACKEND_CALLOC(n_elements, elem_size);
+	}
 
-  if (n_elements == 0 || elem_size == 0)
-  {
-    return nullptr;
-  }
-  size_t req = n_elements * elem_size;
-  if (req / n_elements != elem_size)
-  {
-    return nullptr;
-  }
-  void *mem = MALLOC(req);
-  if (mem != nullptr)
-  {
-    memset(mem, 0, req);
-  }
-  return mem;
+	if (n_elements == 0 || elem_size == 0)
+	{
+		return nullptr;
+	}
+	size_t req = n_elements * elem_size;
+	if (req / n_elements != elem_size)
+	{
+		return nullptr;
+	}
+	void *mem = MALLOC(req);
+	if (mem != nullptr)
+	{
+		memset(mem, 0, req);
+	}
+	return mem;
 }
 
 void *REALLOC(void *oldmem, size_t bytes)
 {
-  if (not asan_inited)
-  {
-    return BACKEND_REALLOC(oldmem, bytes);
-  }
+	if (not asan_inited)
+	{
+		return BACKEND_REALLOC(oldmem, bytes);
+	}
 
-  void *mem = 0;
-  if (oldmem == nullptr)
-  {
-    return MALLOC(bytes);
-  }
-  if (bytes == 0)
-  {
-    FREE(oldmem);
-    return nullptr;
-  }
+	void *mem = 0;
+	if (oldmem == nullptr)
+	{
+		return MALLOC(bytes);
+	}
+	if (bytes == 0)
+	{
+		FREE(oldmem);
+		return nullptr;
+	}
 
-  mem = MALLOC(bytes);
+	mem = MALLOC(bytes);
 
-  if (mem != 0)
-  {
-    uptr chunk_beg = reinterpret_cast<uptr>(oldmem) - sizeof(chunk);
-    chunk *m = reinterpret_cast<chunk *>(chunk_beg);
-    size_t old_size = m->user_size;
+	if (mem != 0)
+	{
+		uptr chunk_beg = reinterpret_cast<uptr>(oldmem) - sizeof(chunk);
+		chunk *m = reinterpret_cast<chunk *>(chunk_beg);
+		size_t old_size = m->user_size;
 
-    memcpy(mem, oldmem, bytes > old_size ? old_size : bytes);
-    FREE(oldmem);
-  }
+		memcpy(mem, oldmem, bytes > old_size ? old_size : bytes);
+		FREE(oldmem);
+	}
 
-  return mem;
+	return mem;
 }
