8e566aec1d84bb90a947828500795967750f83cb
LihengChen <791960492@qq.com>
2022-06-14 18:32:07 +0800

    speed up SLSan runtime

--- SGXSanRT/SGXSanRTEnclave/ThreadFuncArgShadowStack.cpp
+++ SGXSanRT/SGXSanRTEnclave/ThreadFuncArgShadowStack.cpp
@@ -2,24 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-#include <unordered_map>
-#include <unordered_set>
 #include <algorithm>
 #include <stack>
 #include "ThreadFuncArgShadowStack.hpp"
 #include "SGXSanLog.hpp"
 
-// function addr -> poisoned function argument index set
-// ( -1 means return value)
+// function addr -> poisoned function argument map [-1,0,1,...,MAX_ARG_NUM-2] ( -1 means return value)
+#define MAX_ARG_NUM 320 // must be multiple of 64
+#define MAX_STACK_DEPTH 50000
 struct FuncArgShadowTy
 {
     uint64_t func_addr;
-    std::unordered_set<int64_t> arg_shadow;
-    FuncArgShadowTy(uint64_t &_func_addr, std::unordered_set<int64_t> &_arg_shadow)
-    {
-        func_addr = _func_addr;
-        arg_shadow = _arg_shadow;
-    }
+    bool arg_shadow[MAX_ARG_NUM];
 };
 
 class ThreadFuncArgShadowStack
@@ -37,77 +31,71 @@ public:
     static void show_arg_shadow(uint64_t func_addr);
 
 private:
-    static __thread std::stack<FuncArgShadowTy> *thread_func_arg_shadow_stack;
+    static __thread FuncArgShadowTy thread_func_arg_shadow_stack[MAX_STACK_DEPTH];
+    static __thread size_t thread_func_arg_shadow_stack_current_depth;
 };
-__thread std::stack<FuncArgShadowTy> *ThreadFuncArgShadowStack::thread_func_arg_shadow_stack;
+__thread FuncArgShadowTy ThreadFuncArgShadowStack::thread_func_arg_shadow_stack[MAX_STACK_DEPTH];
+__thread size_t ThreadFuncArgShadowStack::thread_func_arg_shadow_stack_current_depth;
 
 // a serial of c wrapper for instrumentation
 void ThreadFuncArgShadowStack::init()
 {
-    thread_func_arg_shadow_stack = new std::stack<FuncArgShadowTy>();
+    thread_func_arg_shadow_stack_current_depth = 0;
 }
 
 void ThreadFuncArgShadowStack::destroy()
 {
-    delete thread_func_arg_shadow_stack;
-    thread_func_arg_shadow_stack = nullptr;
+    thread_func_arg_shadow_stack_current_depth = 0;
 }
 
 void ThreadFuncArgShadowStack::poison_arg(uint64_t func_addr, int64_t arg_pos)
 {
-    uint64_t stack_size = thread_func_arg_shadow_stack->size();
-
-    if (stack_size == 0 ||
-        thread_func_arg_shadow_stack->top().func_addr != func_addr)
-    {
-        // the caller maybe uninstrumented since it's a third-party library's function
-        // so there is no shadow stack frame for current function
+    assert(-1 <= arg_pos && arg_pos <= (MAX_ARG_NUM - 2));
+    // There is no shadow stack frame for current function (stack_depth == 0 or top.func_addr != func_addr):
+    // 1) the caller maybe uninstrumented since it's a third-party library's function (ret case)
+    if (thread_func_arg_shadow_stack_current_depth == 0)
         return;
-    }
-    FuncArgShadowTy &correct_top = thread_func_arg_shadow_stack->top();
-    assert(correct_top.func_addr == func_addr);
-    correct_top.arg_shadow.emplace(arg_pos);
+    auto &top = thread_func_arg_shadow_stack[thread_func_arg_shadow_stack_current_depth - 1];
+    if (top.func_addr != func_addr)
+        return;
+    top.arg_shadow[arg_pos + 1] = true;
     // show_arg_shadow(func_addr);
 }
 
 void ThreadFuncArgShadowStack::unpoison_arg(uint64_t func_addr, int64_t arg_pos)
 {
-    uint64_t stack_size = thread_func_arg_shadow_stack->size();
-    assert(stack_size > 0);
-    FuncArgShadowTy &top = thread_func_arg_shadow_stack->top();
+    assert(-1 <= arg_pos && arg_pos <= (MAX_ARG_NUM - 2));
+    assert(thread_func_arg_shadow_stack_current_depth > 0);
+    auto &top = thread_func_arg_shadow_stack[thread_func_arg_shadow_stack_current_depth - 1];
     assert(top.func_addr == func_addr);
-    top.arg_shadow.erase(arg_pos);
+    top.arg_shadow[arg_pos + 1] = false;
 }
 
 void ThreadFuncArgShadowStack::show_arg_shadow(uint64_t func_addr)
 {
-    FuncArgShadowTy &top = thread_func_arg_shadow_stack->top();
+    auto &top = thread_func_arg_shadow_stack[thread_func_arg_shadow_stack_current_depth - 1];
     assert(top.func_addr == func_addr);
-    std::unordered_set<int64_t> &arg_shadow = top.arg_shadow;
     log_trace("[ Argument Shadow of 0x%lx ]:", func_addr);
-    for (int64_t shadow_item : arg_shadow)
+    for (auto shadow_item : top.arg_shadow)
     {
-        log_trace(" %ld", shadow_item);
+        log_trace(" %d", shadow_item);
     }
     log_trace(" \n");
 }
 
 bool ThreadFuncArgShadowStack::query_arg(uint64_t func_addr, int64_t arg_pos)
 {
-    uint64_t stack_size = thread_func_arg_shadow_stack->size();
-
-    if (stack_size == 0 ||
-        thread_func_arg_shadow_stack->top().func_addr != func_addr)
-    {
-        // the caller maybe uninstrumented since it's a third-party library's function,
-        // or there is no inter-procedure shadow propagation at that caller-callee, but exists at other caller-callee pairs
-        // then there is no shadow stack frame for current function
+    assert(-1 <= arg_pos && arg_pos <= (MAX_ARG_NUM - 2));
+    // There is no shadow stack frame for current function (stack_depth == 0 or top.func_addr != func_addr):
+    // 1) the caller maybe uninstrumented since it's a third-party library's function (arg case)
+    // 2) there is no inter-procedure shadow propagation at that caller-callee, but exists at other caller-callee pairs (arg case)
+    if (thread_func_arg_shadow_stack_current_depth == 0)
         return false;
-    }
-    FuncArgShadowTy &correct_top = thread_func_arg_shadow_stack->top();
-    assert(correct_top.func_addr == func_addr);
+    auto &top = thread_func_arg_shadow_stack[thread_func_arg_shadow_stack_current_depth - 1];
+    if (top.func_addr != func_addr)
+        return false;
     // show_arg_shadow(func_addr);
-    return correct_top.arg_shadow.count(arg_pos) != 0;
+    return top.arg_shadow[arg_pos + 1];
 }
 
 bool ThreadFuncArgShadowStack::onetime_query_arg(uint64_t func_addr, int64_t arg_pos)
@@ -119,27 +107,36 @@ bool ThreadFuncArgShadowStack::onetime_query_arg(uint64_t func_addr, int64_t arg
 
 void ThreadFuncArgShadowStack::clear_frame(uint64_t func_addr)
 {
-    uint64_t stack_size = thread_func_arg_shadow_stack->size();
-    assert(stack_size > 0);
-    FuncArgShadowTy &top = thread_func_arg_shadow_stack->top();
+    sgxsan_error(thread_func_arg_shadow_stack_current_depth == 0 || thread_func_arg_shadow_stack_current_depth > MAX_STACK_DEPTH, "Break down thread_func_arg_shadow_stack");
+    FuncArgShadowTy &top = thread_func_arg_shadow_stack[thread_func_arg_shadow_stack_current_depth - 1];
     assert(top.func_addr == func_addr);
-    top.arg_shadow.clear();
+    // clear arg_shadow
+    uint64_t *p = (uint64_t *)top.arg_shadow;
+    for (size_t step = 0; step < (MAX_ARG_NUM / 64); step++)
+    {
+        p[step] = 0;
+    }
 }
 
 void ThreadFuncArgShadowStack::push_frame(uint64_t func_addr)
 {
-    std::unordered_set<int64_t> new_arg_shadow;
-    new_arg_shadow.clear();
-    thread_func_arg_shadow_stack->emplace(func_addr, new_arg_shadow);
+    thread_func_arg_shadow_stack_current_depth++;
+    sgxsan_error(thread_func_arg_shadow_stack_current_depth > MAX_STACK_DEPTH, "Exceed ceiling of thread_func_arg_shadow_stack");
+    auto &top = thread_func_arg_shadow_stack[thread_func_arg_shadow_stack_current_depth - 1];
+    top.func_addr = func_addr;
+    // clear arg_shadow
+    uint64_t *p = (uint64_t *)top.arg_shadow;
+    for (size_t step = 0; step < (MAX_ARG_NUM / 64); step++)
+    {
+        p[step] = 0;
+    }
 }
 
 void ThreadFuncArgShadowStack::pop_frame(uint64_t func_addr)
 {
-    uint64_t stack_size = thread_func_arg_shadow_stack->size();
-    assert(stack_size > 0);
-    FuncArgShadowTy &top = thread_func_arg_shadow_stack->top();
-    assert(top.func_addr == func_addr);
-    thread_func_arg_shadow_stack->pop();
+    sgxsan_error(thread_func_arg_shadow_stack_current_depth == 0, "Under bottom of thread_func_arg_shadow_stack");
+    assert(thread_func_arg_shadow_stack[thread_func_arg_shadow_stack_current_depth - 1].func_addr == func_addr);
+    thread_func_arg_shadow_stack_current_depth--;
 }
 
 // c wrappers
