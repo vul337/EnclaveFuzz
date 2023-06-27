c1d42193bb54e43f6db993f1979f78f9d4ff24c0
leone <791960492@qq.com>
2023-06-28 05:56:49 +0800

    Count ECALL with multi process

--- Tool/CountEnterECall.py
+++ Tool/CountEnterECall.py
@@ -2,35 +2,73 @@
 import argparse
 import os
 import re
+import multiprocessing
+import threading
+import functools
 from tqdm.auto import tqdm
 
 gECallCnt = {}
+updateLock = threading.Lock()
+
+
+def Update(ECallCnt: dict, pbar):
+    updateLock.acquire()
+    try:
+        for key in ECallCnt.keys():
+            if key not in gECallCnt.keys():
+                gECallCnt[key] = 0
+            gECallCnt[key] += ECallCnt[key]
+    finally:
+        updateLock.release()
+        pbar.update()
+
+
+def Count(log_abs, kind) -> dict:
+    pECallCnt = {}
+    with open(log_abs, errors="backslashreplace") as log_file:
+        lines = log_file.readlines()
+        for line in lines:
+            match = re.search(kind + r"\s+(\w+)", line)
+            if match:
+                ECallName = match.group(1)
+                if ECallName in pECallCnt.keys():
+                    pECallCnt[ECallName] += 1
+                else:
+                    pECallCnt[ECallName] = 1
+    return pECallCnt
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Analyse all *.log in <log_dir> to count Enter ECall_XXX")
-    parser.add_argument('log_dir')
-    parser.add_argument('--kind', default="Enter")
+        description="Analyse all *.log in <log_dir> to count Enter ECall_XXX"
+    )
+    parser.add_argument("log_dir")
+    parser.add_argument("--kind", default="Enter")
     args = parser.parse_args()
 
-    for log in tqdm(os.listdir(args.log_dir)):
+    pool = multiprocessing.Pool()
+    log_files = sorted(os.listdir(args.log_dir))
+    pbar = tqdm(total=len(log_files))
+    cb = functools.partial(Update, pbar=pbar)
+    for log in log_files:
         if not log.endswith(r".log"):
+            pbar.update
             continue
         log_abs = os.path.join(args.log_dir, log)
-        if os.path.exists(log_abs):
-            with open(log_abs, errors="backslashreplace") as log_file:
-                lines = log_file.readlines()
-                for line in lines:
-                    match = re.search(args.kind+r"\s+(\w+)", line)
-                    if match:
-                        ECallName = match.group(1)
-                        if ECallName in gECallCnt.keys():
-                            gECallCnt[ECallName] += 1
-                        else:
-                            gECallCnt[ECallName] = 1
+        if not os.path.exists(log_abs):
+            pbar.update
+            continue
+        pool.apply_async(
+            Count,
+            args=(log_abs, args.kind),
+            callback=cb,
+            error_callback=lambda x: print(x),
+        )
+    pool.close()
+    pool.join()
+    pbar.close()
     for ECallName in gECallCnt.keys():
-        print(str(gECallCnt[ECallName])+" "+args.kind+" "+ECallName)
+        print(str(gECallCnt[ECallName]) + " " + args.kind + " " + ECallName)
 
 
 if __name__ == "__main__":
